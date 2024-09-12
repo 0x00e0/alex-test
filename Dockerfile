4f7d820e719975ae0614d78827e3c2a34db54a2e
alex-l <alex.l@binance.com>
2024-09-12 22:54:13 +0800

    add fastapi py

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
-FROM nginx as prod
-COPY index.html /etc/nginx/html/
+FROM python:3.12
+WORKDIR /app
+COPY main.py .
+COPY requirements.txt .
+RUN pip install -r requirements.txt
 
+CMD ["python","main.py"]
---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/api/v1/error")
+async def error_event():
+    logger.error("{'level':'error', 'message': 'path /api/v1/error was triggered'}")
+    return "path /api/v1/error was triggered"
+
+@app.get("/api/v1/panic")
+async def panic_event():
+    logger.error({"level": "panic", "message": "path /api/v1/panic was triggered"})
+    os._exit(1)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
